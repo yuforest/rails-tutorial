de95a5a54c23494d9d33afc68d05ac183350cc78
Yuta Omori <lostforest0@gmail.com>
2018-07-01 10:10:43 +0000

    Inplement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -21,6 +22,6 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                :password_confirmation)
     end
 end
